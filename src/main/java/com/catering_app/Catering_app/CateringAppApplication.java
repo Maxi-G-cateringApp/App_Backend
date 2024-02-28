3081c9be46fce19a1e03f570eeaf2c29bbbc66fe
Sreekanth V <sreekanthv1995@gmail.com>
2024-02-28 12:17:12 +0530

    first commit

---
+++ src/main/java/com/catering_app/Catering_app/CateringAppApplication.java
@@ -0,0 +1,13 @@
+package com.catering_app.Catering_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CateringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CateringAppApplication.class, args);
+	}
+
+}
